ae17aa3354d962cea344fa63ecc9ab3ee59a406a
MatheusTA <matrajano12@gmail.com>
2020-01-08 22:35:56 -0300

    Creating compenent Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matheus Antonino</h1>
+    <h2>Desenvolvedor Front-end</h2>
+    <p>Before you start building your first Gatsby site, you’ll need to familiarize yourself with some core web technol</p>
+  </div>
+)
+
+export default Profile
