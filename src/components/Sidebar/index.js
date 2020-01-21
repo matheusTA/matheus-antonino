f3de2f17c0b9bf721ed5bd542e271aa4fda6db71
MatheusTA <matrajano12@gmail.com>
2020-01-20 21:50:47 -0300

    Creating sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Profile from '../Profile'
+import { SidebarWrapper } from './styles';
+
+export default function Sidebar() {
+  return (
+    <SidebarWrapper>
+      <Profile />
+    </SidebarWrapper>
+  );
+}
