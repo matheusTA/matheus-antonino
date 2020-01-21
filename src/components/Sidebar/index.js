52ebd59d30590f068193e33640fe82e59d65ee7a
MatheusTA <matrajano12@gmail.com>
2020-01-21 01:28:57 -0300

    Update layout

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Profile from '../Profile'
+import SocialLinks from '../SocialLinks'
+import MenuLinks from '../MenuLinks'
 import { SidebarWrapper } from './styles';
 
 export default function Sidebar() {
   return (
     <SidebarWrapper>
       <Profile />
+      <SocialLinks />
+      <MenuLinks />
     </SidebarWrapper>
   );
 }
