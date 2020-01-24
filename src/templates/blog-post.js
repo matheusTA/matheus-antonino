775c8c3d22ab4d026307e2a884f3dca0da2e39de
MatheusTA <matrajano12@gmail.com>
2020-01-24 01:03:12 -0300

    Changing page of post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { graphql } from 'gatsby'
 
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import { PostHeader, PostTitle, PostDescription, PostDate, MainContent, } from '../components/Post/styles'
+
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
 
   return (
-    <>
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <PostHeader>
+        <PostDate>{post.frontmatter.date} * {post.timeToRead} mim de leitura</PostDate>
+        <PostTitle>{post.frontmatter.title}</PostTitle>
+        <PostDescription>{post.frontmatter.description}</PostDescription>
+      </PostHeader>
+      <MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </MainContent>
+    </Layout>
   )
 }
 
@@ -17,8 +30,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
