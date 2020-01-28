b2b7251ea248d2985b3291ae858dbc50bb4cffbe
MatheusTA <matrajano12@gmail.com>
2020-01-27 22:56:38 -0300

    Changing colors hexadecimal for variables

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -75,5 +75,27 @@ const GlobalStyles = createGlobalStyle`
  ::-webkit-scrollbar-thumb {
     background: #dad7d7;
   }
+
+  body.dark {
+    --borders: #38444d;
+    --texts: #8899a6;
+    --postColor: #fff;
+    --highlight: #1fa1f2;
+    --mediumBackground: #192734;
+    --background: #16202c;
+    --white: #fff;
+    --black: #222;
+  }
+
+  body.light {
+    --borders: #dedede;
+    --postColor: #111;
+    --texts: #555555;
+    --highlight: #1fa1f2;
+    --mediumBackground: #f0f0f3;
+    --background: #fff;
+    --white: #fff;
+    --black: #222;
+  }
 `
 export default GlobalStyles
