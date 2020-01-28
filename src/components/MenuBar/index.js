598b23a44bf4748aac186dc0555d455d2091af6c
MatheusTA <matrajano12@gmail.com>
2020-01-27 23:00:27 -0300

    Changing theme the web site

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
@@ -8,6 +8,15 @@ import { Grid } from "styled-icons/boxicons-solid/Grid"
 import { MenuBarGroup, MenuBarItem, MenuBarLink, MenuBarWrapper } from './styles'
 
 export default function MenuBar() {
+  const [theme, setTheme] = useState(null)
+
+  const isDarkMode = theme === "dark"
+
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
   return (
     <MenuBarWrapper>
       <MenuBarGroup>
@@ -25,7 +34,13 @@ export default function MenuBar() {
 
 
       <MenuBarGroup>
-        <MenuBarItem title="Change the theme">
+        <MenuBarItem
+          title="Change the theme"
+          onClick={() => {
+            window.__setPreferredTheme(isDarkMode ? "light" : "dark")
+          }}
+          className={theme}
+        >
           <Light />
         </MenuBarItem>
         <MenuBarItem title="Change the display">
